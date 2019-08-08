8525f83acea37f9b1d241ae1357ab4354d265df7
David Madriz <disow321@gmail.com>
2019-08-07 18:24:07 -0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -14,6 +14,11 @@ $conn = mysql_connect("localhost","root","root")
  */
 
 
+ $bdd = new PDO(
+    	"mysql:host=" . getenv("MYSQL_ADDON_HOST") . ";dbname=" . getenv("MYSQL_ADDON_DB"),
+    	getenv("MYSQL_ADDON_USER"),
+    	getenv("MYSQL_ADDON_PASSWORD")
+  );
 
 $databaseHost = 'b6w7dnsloorszuyrgvfl-mysql.services.clever-cloud.com';
 $databaseName = 'b6w7dnsloorszuyrgvfl';
